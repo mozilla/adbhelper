363525cda5f989c455dc159beee8aacb5801daa9
J. Ryan Stinnett <jryans@gmail.com>
2015-05-28 15:02:01 -0500

    Bug 1026945 - Wait for consumers to signal ADB to start polling. r=ochameau

---
+++ device.js
@@ -0,0 +1,78 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+"use strict";
+
+const { Cu } = require("chrome");
+const { devtools } = Cu.import("resource://gre/modules/devtools/Loader.jsm", {});
+const devtoolsRequire = devtools.require;
+const { ConnectionManager } =
+  devtoolsRequire("devtools/client/connection-manager");
+const adb = require("./adb");
+
+/**
+ * A Device instance is created and registered with the Devices module whenever
+ * ADB notices a new device is connected.
+ *
+ * Any changes here should be examined carefully for backwards compatibility
+ * issues.  Other add-ons, like the Tools Adapter, make use of these low-level
+ * device objects too.
+ */
+function Device(id) {
+  this.id = id;
+}
+
+Device.prototype = {
+  /**
+   * DEPRECATED: This is specific to how we connect to Firefox OS.  Use cases
+   * that interact with other kinds of devices should likely use the more
+   * general |forwardPort| method directly.
+   */
+  connect: function (remotePort) {
+    let port = ConnectionManager.getFreeTCPPort();
+    let local = "tcp:" + port;
+    let remote = "localfilesystem:/data/local/debugger-socket";
+    if (remotePort) {
+      remote = "tcp:" + remotePort;
+    }
+    return adb.forwardPort(local, remote)
+              .then(() => port);
+  },
+
+  type: "adb",
+
+  shell: adb.shell.bind(adb),
+  forwardPort: adb.forwardPort.bind(adb),
+  push: adb.push.bind(adb),
+  pull: adb.pull.bind(adb),
+  reboot: adb.reboot.bind(adb),
+  rebootRecovery: adb.rebootRecovery.bind(adb),
+  rebootBootloader: adb.rebootBootloader.bind(adb),
+
+  isRoot: function() {
+    return adb.shell("id").then(stdout => {
+      if (stdout) {
+        let uid = stdout.match(/uid=(\d+)/)[1];
+        return uid == "0";
+      } else {
+        return false;
+      }
+    });
+  },
+
+  summonRoot: function() {
+    return adb.root();
+  },
+
+  getModel: function() {
+    if (this._modelPromise) {
+      return this._modelPromise;
+    }
+    this._modelPromise = this.shell("getprop ro.product.model")
+                             .then(model => model.trim());
+    return this._modelPromise;
+  }
+};
+
+module.exports = Device;
